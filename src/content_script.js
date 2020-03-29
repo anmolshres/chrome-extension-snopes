cbd124296a0014e8f0524abd45e3fbafe4bf52a8
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-03-29 00:39:43 -0400

    remove extra files from boilerplate that is not used

--- src/content_script.js
+++
@@ -1,8 +0,0 @@
-function searchWithKeyWord() {
-    let clickplanet = document.getElementsByClassName('btn-submit')[0];
-    let inputField = document.getElementById('header-search');
-    inputField.value = "covid-19";
-    clickplanet.click();
-}
-
-searchWithKeyWord();
--- src/popup.js
+++
@@ -1,9 +0,0 @@
-function injectTheScript() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        // query the active tab, which will be only one tab
-        //and inject the script in it
-        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
-    });
-}
-
-document.getElementById('clickactivity').addEventListener('click', injectTheScript);
