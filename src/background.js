dcbd9f34806637cd792453940e2fc487a27b2c35
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-03-28 23:46:18 -0400

    update background.js to visit snopes

--- src/background.js
+++ src/background.js
@@ -1,19 +1,16 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Callback function to visit snopes
+function visitSite(info, tab) {
+    // alert(JSON.stringify(info));
+    // console.log("item " + info.menuItemId + " was clicked");
+    // console.log("info: " + JSON.stringify(info));
+    // console.log("tab: " + JSON.stringify(tab));
+    const url = `https://www.snopes.com/`
+    chrome.tabs.create({ url: url, index: tab.index + 1 });
+}
 
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-    alert(JSON.stringify(info.selectionText));
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("info: " + JSON.stringify(info));
-    console.log("tab: " + JSON.stringify(tab));
-  }
-  
-  // Create one test item for each context type.
-  var contexts = ["selection"];
-    var context = contexts[0];
-    var title = "Test '" + context + "' menu item";
-    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "onclick": genericOnClick});
-    console.log("'" + context + "' item:" + id);
+const context = "selection";
+const title = "Search Snopes for selected phrase";
+const id = chrome.contextMenus.create({
+    "title": title, "contexts": [context],
+    "onclick": visitSite
+});
