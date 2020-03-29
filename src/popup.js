c1c85302fbe77df5b66e1fd9eea1055933ebc138
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-03-29 15:38:04 -0400

    add comments to popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,29 @@
+// when the popup page is loaded
 document.addEventListener('DOMContentLoaded', initial);
 
+/**
+ * Initial function to run when the popup page is loaded. This function adds click event listener to submit button.
+ * @returns void
+ */
 function initial() {
     const submitButton = document.getElementById('submit-btn');
     submitButton.addEventListener('click', handleSearchClick);
 }
 
+/**
+ * This function handles search button click event and calls function visitAndSearchSnopes.
+ * @returns void
+ */
 function handleSearchClick() {
     const query = document.getElementById('search-query').value;
     visitAndSearchSnopes(query);
 }
 
+/**
+ * This function visits Snopes in a new tab and searches for user entered query.
+ * @param query string to be lookeup in Snopes
+ * @returns void
+ */
 function visitAndSearchSnopes(query) {
     const url = `https://www.snopes.com/?s=${query}`
     chrome.tabs.create({ url: url});
