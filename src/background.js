7b020ce35967fa0bbb7ddf024dd35d2ed1d32c9e
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-03-29 04:24:33 -0400

    add comments to code

--- src/background.js
+++ src/background.js
@@ -1,9 +1,10 @@
-// Callback function to visit snopes
+// Callback function to create new tab and visit snopes when context menu item is clicked
 function visitSite(info, tab) {
     const url = `https://www.snopes.com/?s=${info.selectionText}`
     chrome.tabs.create({ url: url, index: tab.index + 1 });
 }
 
+// create contextmenu item listing the availability to search Snopes for highlighted text
 const title = `Search Snopes for "%s"`;
 chrome.contextMenus.create({
     "title": title, "contexts": ["selection"],
