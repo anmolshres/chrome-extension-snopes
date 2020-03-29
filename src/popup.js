1ad258772213924bbc76bedfe4bdfa76bf971b2c
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-03-29 15:31:49 -0400

    edit input field id and add search snopes functionality in popup

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', initial);
 
-function initial(){
+function initial() {
     const submitButton = document.getElementById('submit-btn');
-    submitButton.addEventListener('click',handleSearchClick);
+    submitButton.addEventListener('click', handleSearchClick);
 }
 
+function handleSearchClick() {
+    const query = document.getElementById('search-query').value;
+    visitAndSearchSnopes(query);
+}
+
+function visitAndSearchSnopes(query) {
+    const url = `https://www.snopes.com/?s=${query}`
+    chrome.tabs.create({ url: url});
+}
