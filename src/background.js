93b427da5711367fada67d99499a49afba77d15a
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-03-29 01:16:27 -0400

    minor code cleanup and update title on right click

--- src/background.js
+++ src/background.js
@@ -1,16 +1,11 @@
 // Callback function to visit snopes
 function visitSite(info, tab) {
-    // alert(JSON.stringify(info));
-    // console.log("item " + info.menuItemId + " was clicked");
-    // console.log("info: " + JSON.stringify(info));
-    // console.log("tab: " + JSON.stringify(tab));
     const url = `https://www.snopes.com/?s=${info.selectionText}`
     chrome.tabs.create({ url: url, index: tab.index + 1 });
 }
 
-const context = "selection";
-const title = "Search Snopes for selected phrase";
-const id = chrome.contextMenus.create({
-    "title": title, "contexts": [context],
+const title = `Search Snopes for "%s"`;
+chrome.contextMenus.create({
+    "title": title, "contexts": ["selection"],
     "onclick": visitSite
 });
